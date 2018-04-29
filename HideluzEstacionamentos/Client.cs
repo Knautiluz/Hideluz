29ff78e224db0bfdb475154a4004e7138be5d80c
unknown <bleed-it-out@live.com>
2018-04-29 17:36:40 -0300

    feat(HideluzEstionamentos) : added classes client, address, tax and vehicle

---
+++ HideluzEstacionamentos/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideluzEstacionamentos
+{
+    public class Client
+    {
+        public string Document { get; set; }
+        public string Name { get; set; }
+        Address Address = new Address();
+        public string Email { get; set; }
+        public string Type { get; set; }
+
+        public Client()
+        {
+            // constructor
+        }
+    }
+}
---
+++ HideluzEstacionamentos/Tax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideluzEstacionamentos
+{
+    public class Tax
+    {
+        public double PricePerHour { get; set; }
+        public DateTime EntryTime { get; set; }
+        public DateTime DepartureTime { get; set; }
+
+        public Tax()
+        {
+            // constructor
+        }
+
+        public double CalculateTax(double PricePerHour, TimeSpan EntryTime, TimeSpan DepartureTime)
+        {
+            // lógica do método
+            return 0.1;
+        }
+    }
+}
