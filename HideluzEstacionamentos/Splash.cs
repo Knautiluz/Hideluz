25a557ce367f14c9a5c7001856c9d75d4965a817
Jackson Jones <knautiluz@gmail.com>
2018-05-25 17:26:51 -0300

    #update

---
+++ HideluzEstacionamentos/Splash.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HideluzEstacionamentos
+{
+    public partial class Splash : Form
+    {
+        public static bool ProgressComplete;
+        public static List<string> Types = new List<string>();
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void Splash_Load(object sender, EventArgs e)
+        {
+            SplashProgressBar.PerformStep();
+            ProgressComplete = true;
+        }
+    }
+}
