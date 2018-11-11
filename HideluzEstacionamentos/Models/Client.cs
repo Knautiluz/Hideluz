fc6da05cfc16be9ea6c5decf78fc9ffdb9b21621
m-ssilva <bleed-it-out@live.com>
2018-11-10 23:29:18 -0200

    refactor(model): removed comments and refactoring structure

--- HideluzEstacionamentos/Models/Client.cs
+++ HideluzEstacionamentos/Models/Client.cs
@@ -1,35 +1,20 @@
-﻿namespace HideluzEstacionamentos
+﻿using HideluzEstacionamentos.Models;
+
+namespace HideluzEstacionamentos
 {
     public class Client
     {
         public string Document { get; set; }
-        // documento do cliente (cpf)
         public string Name { get; set; }
-        // nome do cliente
         public string Email { get; set; }
-        // e-mail do cliente
-        public string Tel { get; set; }
-        // telefone do cliente
-        public int Type { get; set; }
-        // tipo do cliente mensalista / avulso
+        public string Phone { get; set; }
+        public int IdType { get; set; }
+        public Address Address { get; set; }
 
         public Client()
         {
-            // constructor
-        }
-        public class Address
-        {
-            public string State { get; set; }
-            public string City { get; set; }
-            public string Street { get; set; }
-            public string Number { get; set; }
-            public string Neighborhood { get; set; }
-            public string Cep { get; set; }
-
-            public Address()
-            {
-                // constructor
-            }
+            Address Address = new Address();
+            this.Address = Address;
         }
     }
 }
