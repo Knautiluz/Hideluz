08136cb275d8c36fc91faeb549169aff63607c87
m-ssilva <bleed-it-out@live.com>
2018-11-15 20:59:22 -0200

    feat(DAO): add login DAO

---
+++ HideluzEstacionamentos/DAO/LoginDAO.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace HideluzEstacionamentos.DAO
+{
+    public class LoginDAO : Connection
+    {
+
+        MySqlCommand command = null;
+
+        public bool Select(Login login)
+        {
+            try
+            {
+                OpenConnection();
+                command = new MySqlCommand("SELECT count(*) FROM users WHERE username = @username and password = @password", connection);
+                command.Parameters.AddWithValue("@username", login.Username);
+                command.Parameters.AddWithValue("@password", login.Password);
+
+                var count = command.ExecuteScalar();
+
+                if (Convert.ToInt32(count) > 0)
+                {
+                    return true;
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+        }
+    }
+}
