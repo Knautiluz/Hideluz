ddb91ff5920cea521cb2ab566915eb0939c25c22
m-ssilva <bleed-it-out@live.com>
2018-11-25 23:21:41 -0200

    refactor(DAO): adjustments in DAO for new fields in DB

--- HideluzEstacionamentos/DAO/ClientDAO.cs
+++ HideluzEstacionamentos/DAO/ClientDAO.cs
@@ -15,19 +15,20 @@ namespace HideluzEstacionamentos.DAO
                 OpenConnection();
 
                 command = new MySqlCommand(
-                            "INSERT INTO clientes (cpf, nome, estado, cidade, bairro, rua, numero, cep, email, telContato, idTipoCliente)" +
-                            "VALUES (@document, @name, @state, @city, @neighborhood, @street, @number, @zipcode, @email, @phone, @type)", connection);
+                            "INSERT INTO clientes (tx_cpf, tx_nome, tx_email, tx_telefone, id_tipocliente, tx_estado, tx_cidade, tx_bairro, tx_rua, tx_numero, tx_cep, fl_ativo)" +
+                            "VALUES (@document, @name, @email, @phone, @type, @state, @city, @neighborhood, @street, @number, @zipcode, @status)", connection);
                 command.Parameters.AddWithValue("@document", client.Document);
                 command.Parameters.AddWithValue("@name", client.Name);
-                command.Parameters.AddWithValue("@state", client.Email);
+                command.Parameters.AddWithValue("@email", client.Email);
+                command.Parameters.AddWithValue("@phone", client.Phone);
+                command.Parameters.AddWithValue("@type", client.IdType);
+                command.Parameters.AddWithValue("@state", client.Address.State);
                 command.Parameters.AddWithValue("@city", client.Address.City);
                 command.Parameters.AddWithValue("@neighborhood", client.Address.Neighborhood);
                 command.Parameters.AddWithValue("@street", client.Address.Street);
                 command.Parameters.AddWithValue("@number", client.Address.Number);
                 command.Parameters.AddWithValue("@zipcode", client.Address.ZIPCode);
-                command.Parameters.AddWithValue("@email", client.Email);
-                command.Parameters.AddWithValue("@phone", client.Phone);
-                command.Parameters.AddWithValue("@type", client.IdType);
+                command.Parameters.AddWithValue("@status", 1);
 
                 command.ExecuteNonQuery();
             }
