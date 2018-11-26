8b352fa942fe44b75dc09e0d87c09c238296857d
m-ssilva <bleed-it-out@live.com>
2018-11-25 23:23:26 -0200

    feat(DAO): new DAOmethods for client

--- HideluzEstacionamentos/DAO/ClientDAO.cs
+++ HideluzEstacionamentos/DAO/ClientDAO.cs
@@ -44,6 +44,91 @@ namespace HideluzEstacionamentos.DAO
             }
         }
 
+        public Client SearchByDocument(Client client)
+        {
+            try
+            {
+                OpenConnection();
+                command = new MySqlCommand("SELECT * FROM clientes where tx_cpf = @ClientDocument LIMIT 1", connection);
+                command.Parameters.AddWithValue("@ClientDocument", client.Document);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    client.Document = reader["tx_cpf"].ToString();
+                    client.Name = reader["tx_nome"].ToString();
+                    client.Email = reader["tx_email"].ToString();
+                    client.Phone = reader["tx_telefone"].ToString();
+                    client.IdType = Convert.ToInt32(reader["id_tipocliente"].ToString());
+                    client.Address.State = reader["tx_estado"].ToString();
+                    client.Address.City = reader["tx_cidade"].ToString();
+                    client.Address.Neighborhood = reader["tx_bairro"].ToString();
+                    client.Address.Street = reader["tx_rua"].ToString();
+                    client.Address.Number = reader["tx_numero"].ToString();
+                    client.Address.ZIPCode = reader["tx_cep"].ToString();
+                    client.CreatedDate = Convert.ToDateTime(reader["dt_criacao"].ToString());
+                    client.Status = Convert.ToBoolean(reader["fl_ativo"]);
+                }
+
+                return client;
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+        }
+
+        public void UpdateClient(Client OldClient, Client UpdatedClient)
+        {
+            try
+            {
+                OpenConnection();
+                command = new MySqlCommand("UPDATE clientes set tx_cpf = @ClientDocument, tx_nome = @ClientName, tx_email = @ClientEmail," +
+                    "tx_telefone = @ClientPhone, id_tipocliente = @ClientType, tx_estado = @ClientState, tx_cidade = @ClientCity," +
+                    "tx_bairro = @ClientNeighborhood, tx_rua = @ClientStreet, tx_numero = @ClientNumber, tx_cep = @ClientZIPCode," +
+                    "fl_ativo = @ClientStatus WHERE tx_cpf = @OldClientDocument", connection);
+                command.Parameters.AddWithValue("@OldClientDocument", OldClient.Document);
+                command.Parameters.AddWithValue("@ClientDocument", UpdatedClient.Document);
+                command.Parameters.AddWithValue("@ClientName", UpdatedClient.Name);
+                command.Parameters.AddWithValue("@ClientEmail", UpdatedClient.Email);
+                command.Parameters.AddWithValue("@ClientPhone", UpdatedClient.Phone);
+                command.Parameters.AddWithValue("@ClientType", UpdatedClient.IdType);
+                command.Parameters.AddWithValue("@ClientState", UpdatedClient.Address.State);
+                command.Parameters.AddWithValue("@ClientCity", UpdatedClient.Address.City);
+                command.Parameters.AddWithValue("@ClientNeighborhood", UpdatedClient.Address.Neighborhood);
+                command.Parameters.AddWithValue("@ClientStreet", UpdatedClient.Address.Street);
+                command.Parameters.AddWithValue("@ClientNumber", UpdatedClient.Address.Number);
+                command.Parameters.AddWithValue("@ClientZIPCode", UpdatedClient.Address.ZIPCode);
+                command.Parameters.AddWithValue("@ClientStatus", UpdatedClient.Status);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+        }
+
+        public bool CheckClientExists(Client client)
+        {
+            try
+            {
+                OpenConnection();
+                command = new MySqlCommand("SELECT COUNT(*) FROM clientes where tx_cpf = @ClientDocument LIMIT 1", connection);
+                command.Parameters.AddWithValue("@ClientDocument", client.Document);
+                var count = Convert.ToInt32(command.ExecuteScalar());
+                if (count >= 1) { return true; }
+                else { return false; }
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+        }
+
         public MySqlDataReader FillClientType()
         {
 
