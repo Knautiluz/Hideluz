1a9a68944054c3ebed9badf103fe5e7a30e3ef65
m-ssilva <bleed-it-out@live.com>
2018-11-30 00:18:57 -0200

    refactor(view): ux adjustments

--- HideluzEstacionamentos/Views/FormMain.cs
+++ HideluzEstacionamentos/Views/FormMain.cs
@@ -36,16 +36,16 @@ namespace HideluzEstacionamentos
             if (Controller.ValidateUser(InputLogin))
             {
                 Controller.FillUserData(InputLogin);
-                MessageBox.Show("Login realizado com sucesso!", "Usuário logado.", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 Splash.Close();
+                MessageBox.Show("Login realizado com sucesso!", "Usuário logado.", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 FormLoggerv2 formLogged = new FormLoggerv2(InputLogin);
                 formLogged.Show();
             }
 
             else
             {
-                MessageBox.Show("Dados inválidos", "Falha ao tentar logar.", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 Splash.Close();
+                MessageBox.Show("Dados inválidos", "Falha ao tentar logar.", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 var form = Application.OpenForms[0];
                 form.Show();
             }
