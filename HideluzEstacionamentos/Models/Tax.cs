238d4f8c8229f867de548903b9744daef6a7bbf4
m-ssilva <bleed-it-out@live.com>
2018-12-05 20:43:59 -0200

    feat(model): adding tax model

---
+++ HideluzEstacionamentos/Models/Tax.cs
@@ -0,0 +1,10 @@
+﻿namespace HideluzEstacionamentos.Models
+{
+    public class Tax
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public int IdType { get; set; }
+        public int IdVehicleType { get; set; }
+    }
+}
